a98c0b07523d6ac6575834c32a3d415297e6f54d
Hyunsoo Jeong <hyeonsuj48@gmail.com>
2024-07-07 23:37:48 +0900

    path commit

--- video_analysis/gen_viz.py
+++ video_analysis/gen_viz.py
@@ -4,23 +4,61 @@ from .team_heatmap import TeamHeatmap
 from .hasball_report import PossessionReport
 
 
+# def basic_gen(track_stub_path):
+#     with open(track_stub_path, "rb") as load1:
+#         tracks = pickle.load(load1)
+#     match_id = "testMatch"
+#     base_pitch_path = "test/img/nuri_futsal.png"
+#     heatmap_path_list = TeamHeatmap().gen_team_heatmap(
+#         tracks, base_pitch_path, match_id, "viz/heatmap_team"
+#     )
+#
+#     hasball_report = PossessionReport(
+#         "df/heatmap/testMatch-heatmap-df.csv", base_pitch_path
+#     )
+#     possession_lmr_path_list = hasball_report.visual_possession(
+#         match_id, "viz/possession"
+#     )
+#     possession_dmr_path_list = hasball_report.visual_activate_zone(
+#         match_id, "viz/possession"
+#     )
+#     path_dict = {
+#         "heatmap_home": heatmap_path_list[0],
+#         "heatmap_away": heatmap_path_list[1],
+#         "hasball_lmr_home": possession_lmr_path_list[0],
+#         "hasball_lmr_away": possession_lmr_path_list[1],
+#         "hasball_dmr": possession_dmr_path_list[0],
+#     }
+#     # 이미지 경로를 MEDIA_URL 기반으로 변경
+#     for key, value in path_dict.items():
+#         path_dict[key] = os.path.join("viz", value)
+#
+#     return path_dict
+#
+#
+# if __name__ == "__main__":
+#     basic_gen()
+
+
 def basic_gen(track_stub_path):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
     with open(track_stub_path, "rb") as load1:
         tracks = pickle.load(load1)
     match_id = "testMatch"
-    base_pitch_path = "test/img/nuri_futsal.png"
+    base_pitch_path = os.path.join(base_dir, "test/img/nuri_futsal.png")
     heatmap_path_list = TeamHeatmap().gen_team_heatmap(
-        tracks, base_pitch_path, match_id, "viz/heatmap_team"
+        tracks, base_pitch_path, match_id, os.path.join(base_dir, "viz/heatmap_team")
     )
 
     hasball_report = PossessionReport(
-        "df/heatmap/testMatch-heatmap-df.csv", base_pitch_path
+        os.path.join(base_dir, "df/heatmap/testMatch-heatmap-df.csv"), base_pitch_path
     )
     possession_lmr_path_list = hasball_report.visual_possession(
-        match_id, "viz/possession"
+        match_id, os.path.join(base_dir, "viz/possession")
     )
     possession_dmr_path_list = hasball_report.visual_activate_zone(
-        match_id, "viz/possession"
+        match_id, os.path.join(base_dir, "viz/possession")
     )
     path_dict = {
         "heatmap_home": heatmap_path_list[0],
@@ -29,7 +67,6 @@ def basic_gen(track_stub_path):
         "hasball_lmr_away": possession_lmr_path_list[1],
         "hasball_dmr": possession_dmr_path_list[0],
     }
-    # 이미지 경로를 MEDIA_URL 기반으로 변경
     for key, value in path_dict.items():
         path_dict[key] = os.path.join("viz", value)
 
