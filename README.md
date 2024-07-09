dba05712379c35b24c6d539db967d2fcc8460664
Hyunsoo Jeong <hyeonsuj48@gmail.com>
2024-07-09 12:06:27 +0900

    readme

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # DA34-final-Football_Agora_Web_Video
+
+## Adios Team Final_Project (팀 프로젝트)
+
+### Django 프레임워크
+
+### 사용언어 및 라이브러리
+- 언어: Python, Javascript
+- 운영서버 라이브러리: [prod.txt](requirements/common.txt)
+- 개발서버 라이브러리: [dev.txt](requirements/dev.txt)
+
+
+### 웹 서비스 기능
+1. 기본적인 웹페이지 기능: 
+- **관련 앱 URL 정의**: [accounts앱](accounts/urls.py)
+   1. 회원가입, 회원탈퇴, 로그인/로그아웃, 아이디/비밀번호 찾기, 비밀번호 초기화(이메일 인증) 후 변경
+      <details>
+        <summary>예시 화면</summary>
+        <img src="FA_화면/회원가입.jpg" alt="회원가입">
+      </details>
+     
+   2. 메인화면, 마이페이지
+      <details>
+        <summary>예시 화면</summary>
+        <img src="FA_화면/FA_메인화면.jpg" alt="메인페이지">
+        <img src="FA_화면/마이페이지.jpg" alt="마이페이지">
+      </details>
+      
+2. 풋살 매칭 플랫폼 마련:
+- **관련 앱 URL 정의**: [team앱](team/urls.py)
+  1. 팀 생성/가입/탈퇴/수정
+     <details>
+       <summary>예시 화면</summary>
+       <img src="FA_화면/팀생성,가입화면.jpg" alt="팀생성/가입">
+       <img src="FA_화면/팀생성.jpg" alt="팀생성">
+       <img src="FA_화면/팀목록.jpg" alt="팀가입">
+     </details>
+  2. 팀매칭(Team Match): 팀매칭을 위한 경기생성/참여
+     <details>
+       <summary>예시 화면</summary>
+       <img src="FA_화면/FA_팀매치.jpg" alt="경기 목록/참여">
+       <img src="FA_화면/경기생성.jpg" alt="경기 생성">
+     </details>
+  3. 팀페이지(My Team): 팀 순위, 팀 경기일정
+     <details>
+       <summary>예시 화면</summary>
+       <img src="FA_화면/my_team페이지.jpg" alt="팀 페이지">
+     </details>
+  4. 팀 경기 결과 작성: 경기리포트 작성/수정/삭제
+     <details>
+       <summary>예시 화면</summary>
+       <img src="FA_화면/경기%20리포트%20작성.jpg" alt="경기리포트 작성">
+     </details>
+  5. 매칭멤버 구하기: 팀 게시판 CRUD
+     <details>
+       <summary>예시 화면</summary>
+       <img src="FA_화면/팀게시판.jpg" alt="팀 게시판">
+     </details>
+  6. 팀스토리(Team Story): FA 전체팀들의 순위, 경기일정
+     <details>
+       <summary>예시 화면</summary>
+       <img src="FA_화면/FA_팀스토리.jpg" alt="팀 스토리">
+     </details>
+      
+3. Chatbot을 이용한 풋살에 관한 접근성 향상
+- **관련 앱 URL 정의**: [chatbot앱](chatbot/urls.py)
+  1. FastApi를 이용한 모델 서빙 [모델서빙](chatbot/views.py)
+     <details>
+       <summary>예시 화면</summary>
+       <img src="FA_화면/챗봇.jpg" alt="챗봇">
+     </details>
+
+4. 경기 리포트에 올린 경기 동영상을 기반한 영상분석
+   1. 장고내에 영상분석 모델 내장
+      <details>
+          <summary>예시 화면</summary>
+          <img src="FA_화면/영상분석%20이미지.png" alt="영상분석">
+      </details>
+  
--- team/urls.py
+++ team/urls.py
@@ -19,20 +19,27 @@ urlpatterns = [
     # 팀 가입하기 누르면 팀들 나오고 가입하기까지
     path("team_list/", views.team_list, name="team_list"),
     path("join_team/<int:team_id>/", views.join_team, name="join_team"),
+    # 생성된 경기들 보여주기
     path("matches/", views.match_list, name="match_list"),
+    # 경기 생성하기
     path("matches/create/", views.create_match, name="create_match"),
+    # 경기 참여하기
     path("matches/join/<int:match_id>/", views.join_match, name="join_match"),
+    # 경기 리포트 작성
     path(
         "match_report/<int:team_id>/",
         views.create_match_result,
         name="match_report",
     ),
+    # 경기 리포트 수정
     path(
         "match_report_edit/<int:result_id>/",
         views.edit_match_result,
         name="match_report_edit",
     ),
+    # 팀 경기 일정
     path("match_schedule/", views.match_schedule, name="match_schedule"),
+    # 팀 스토리
     path("team_story/", views.team_story, name="team_story"),
     # path("chat/<int:team_id>/", views.team_chat, name="team_chat"),
     # path(
@@ -40,6 +47,7 @@ urlpatterns = [
     #     MessageListCreateAPIView.as_view(),
     #     name="message-list-create",
     # ),
+    # 팀 경기 영상 분석
     path("video_analysis/<int:team_id>/", views.video_analysis, name="video_analysis"),
     # 영상분석 시작 버튼
     # path(
@@ -47,23 +55,31 @@ urlpatterns = [
     #     views.analyze_video,
     #     name="analyze_video",
     # ),
+    # 팀 탈퇴
     path("leave_team/", views.leave_team, name="leave_team"),
+    # 팀 삭제
     path("delete_team/", views.delete_team, name="delete_team"),
     # 게시판 리스트 url
     path("board/<int:team_id>/", views.board_list, name="board_list"),
+    # 게시글 생성
     path("board/<int:team_id>/create/", views.board_create, name="board_create"),
+    # 게시글 목록
     path("board/<int:team_id>/<int:pk>/", views.board_detail, name="board_detail"),
+    # 게시글 수정
     path(
         "board/<int:team_id>/<int:pk>/update/", views.board_update, name="board_update"
     ),
+    # 게시글 삭제
     path(
         "board/<int:team_id>/<int:pk>/delete/", views.board_delete, name="board_delete"
     ),
+    # 댓글 삭제
     path(
         "board/<int:team_id>/<int:pk>/comment_delete/",
         views.board_comment_delete,
         name="board_comment_delete",
     ),
+    # 댓글 업데이트
     path(
         "board/<int:team_id>/<int:pk>/comment_update/",
         views.board_comment_update,
@@ -74,6 +90,7 @@ urlpatterns = [
     #     views.analysis_result_basic,
     #     name="analysis_result_basic",
     # ),
+    # 영상분석에서 영상분석결과 이미지 경로 전달
     path("viz/<path:path>", views.serve_image, name="serve_image"),
     # path("<path:path>", views.serve_image, name="serve_image"),
 ]
